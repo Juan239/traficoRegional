f3424002147d18891950e20d295b65df787462b5
Francisco <francisco.javier2507@gmail.com>
2023-11-30 15:07:57 -0300

    Registro2

--- app/src/main/java/com/example/traficoregional/MainActivity.java
+++ app/src/main/java/com/example/traficoregional/MainActivity.java
@@ -2,13 +2,55 @@ package com.example.traficoregional;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnRegistrar;
+    ImageView imgFacebook, imgGoogle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Inicializar botones e imágenes
+        btnRegistrar = findViewById(R.id.btnRegistrar);
+        imgFacebook = findViewById(R.id.imgFacebook);
+        imgGoogle = findViewById(R.id.imgGoogle);
+
+        // BOTON REGISTRO
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, registroActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // BOTÓN FACEBOOK
+        imgFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://www.facebook.com";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
+        // BOTÓN GOOGLE
+        imgGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://www.google.com";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/traficoregional/registroActivity.java
+++ app/src/main/java/com/example/traficoregional/registroActivity.java
@@ -3,12 +3,22 @@ package com.example.traficoregional;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class registroActivity extends AppCompatActivity {
+
+
+    EditText nombreCompleto, apellidoCompleto, correo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        nombreCompleto = findViewById(R.id.editTextnombreCompleto);
+        apellidoCompleto = findViewById(R.id.editTextapellidoCompleto);
+        correo = findViewById(R.id.editTextCorreo);
+
+        //BASE DE DATOS
     }
 }
